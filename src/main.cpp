e0bdad6425ba49ace4e7e45cfbea22039dbbd9a0
André Biseth <andre.biseth@visma.com>
2019-02-05 08:34:25 +0100

    Added some code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+#include <mcp_can.h>
+#include <NMEA2000_CAN.h>
+#include <NMEA2000_mcp.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  WiFiManager wifiManager;
 }
 
 void loop() {
