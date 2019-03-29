28d2cb1f69bbedec28afccacae57a111968c8790
André Biseth <andre.biseth@onyxcentersource.com>
2019-03-29 11:03:51 +0100

    Deleted files

--- build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-c:\Users\andre.biseth\Documents\Arduino\BatteryMonitor\build\core\libb64\cdecode.c.o: \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cdecode.c \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cdecode.h \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/stdint.h \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/sys/features.h \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1/tools/sdk/include/newlib/sys/_intsup.h
--- build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-c:\Users\andre.biseth\Documents\Arduino\BatteryMonitor\build\core\libb64\cencode.c.o: \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cencode.c \
- C:\Users\andre.biseth\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1\cores\esp32\libb64\cencode.h
